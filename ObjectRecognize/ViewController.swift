b23df422b361f8287a055c12b2a5e278513eba31
Batıkan Sosun <batikan.sosun@loodos.com>
2019-08-04 12:04:53 +0300

    Initial Commit

---
+++ ObjectRecognize/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ObjectRecognize
+//
+//  Created by Batıkan Sosun on 4.08.2019.
+//  Copyright © 2019 Batıkan Sosun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
